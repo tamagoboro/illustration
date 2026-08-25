b36f8b756ada7bfb53e026bac88ac43a161c72d2
ao-py-hub <2024m032@hokuriku-u.ac.jp>
2026-08-25 20:15:06 +0900

    Googlesearchコンソールの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from 'next'
-import './globals.css' // <-- 追加！
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'クリエイター検索・比較',
   description: 'クリエイターのポートフォリオ検索・比較サービス',
+  // Google Search Console 所有権確認用コードを追加
+  verification: {
+    google: 'ux6pHBdkGJujCh1iPf8N9sQ4-JiPnCTibobcaWsA2sE',
+  },
 }
 
 export default function RootLayout({
